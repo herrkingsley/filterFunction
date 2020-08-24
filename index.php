822f4403ac41c60aaa9c3093f0e9ff499d4f2dcf
Jonas Carboo <jonas.carboo@gmail.com>
2020-08-24 20:34:30 +0200

    testing layout

--- index.php
+++ index.php
@@ -17,13 +17,45 @@
 ?>
 
 <div class="wrapper">
-    <div class="poducts">
+    <div class="products">
         <div class="product">
             <img class="item-full product__img" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_large.png?format=webp&v=1530129458" alt="" srcset="">
-            <p class="item product__title">Armbandsur</p>
-            <p class="item product__price">2500 kr</p>
-            <div class="break"></div>
-            <p class="item product__desc">Armbandsur Tissot mekaniskt</p>
+            <div class="product__info">
+                <p class="item product__title">Armbandsur</p>
+                <p class="item product__price">2500 kr</p>
+                <div class="break"></div>
+                <p class="item product__desc">Armbandsur Tissot mekaniskt</p>
+            </div>
+            
+        </div>
+        <div class="product">
+            <img class="item-full product__img" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_large.png?format=webp&v=1530129458" alt="" srcset="">
+            <div class="product__info">
+                <p class="item product__title">Ring</p>
+                <p class="item product__price">2500 kr</p>
+                <div class="break"></div>
+                <p class="item product__desc">Armbandsur Tissot mekaniskt</p>
+            </div>
+            
+        </div>
+        <div class="product">
+            <img class="item-full product__img" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_large.png?format=webp&v=1530129458" alt="" srcset="">
+            <div class="product__info">
+                <p class="item product__title">Armbandsur</p>
+                <p class="item product__price">2500 kr</p>
+                <div class="break"></div>
+                <p class="item product__desc">Armbandsur Tissot mekaniskt</p>
+            </div>
+            
+        </div>
+        <div class="product">
+            <img class="item-full product__img" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_large.png?format=webp&v=1530129458" alt="" srcset="">
+            <div class="product__info">
+                <p class="item product__title">Armbandsur</p>
+                <p class="item product__price">2500 kr</p>
+                <div class="break"></div>
+                <p class="item product__desc">Armbandsur Tissot mekaniskt</p>
+            </div>
             
         </div>
     </div>
@@ -31,5 +63,3 @@
 </div>
 <img src="" alt="" srcset="">
 
-
-<p>Hello world!</p>
