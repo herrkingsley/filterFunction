d1c39b54344d5890d54e8de5d47bbb7ed01429d0
Jonas Carboo <jonas.carboo@gmail.com>
2020-08-24 21:17:08 +0200

    creating the render class cleaning echo in index

--- classes/render.class.php
+++ classes/render.class.php
@@ -1,18 +1,33 @@
 <?php
 
 class Render {
+
+
+
     public static function renderProduct($product){
+        
+    echo "<div class='product'>
+        <img class='item-full product__img' src='{$product->getImg()}'>
+        <div class='product__info'>
+            <p class='item product__title'>{$product->getTitle()}</p>
+            <p class='item product__price'>{$product->getPrice()} kr</p>
+            <div class='break'></div>
+            <p class='item product__desc'>{$product->getDesc()}</p>
+            <p class='item product__desc'>" . self::renderTags($product) . "</p>
+        </div>   
+    </div>";
+    
+    }
 
-        echo "<h2>{$product->getTitle()}</h2>";
-        echo "<p>{$product->getDesc()}</p>";
-        echo "<img src='{$product->getImg()}' alt='' srcset=''>";
-        echo "<p>{$product->getPrice()} kr</p>";
-        echo "<p>{$product->getID()}</p>";
-        echo "<br>";
-        foreach($product->getTags() as $tag){
-            echo "{$tag} <br>";
+    public function renderTags($product) {
+       $output ="";
+        foreach($product->getTags() as $key => $tag){
+           $output .= "<a href='#'>{$tag}</a>";
+           if($key < (count($product->getTags()) - 1 )) {
+            $output .= ", ";
+           }
         }
-
+        return $output;
     }
 }
 
--- index.php
+++ index.php
@@ -18,46 +18,11 @@
 
 <div class="wrapper">
     <div class="products">
-        <div class="product">
-            <img class="item-full product__img" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_large.png?format=webp&v=1530129458" alt="" srcset="">
-            <div class="product__info">
-                <p class="item product__title">Armbandsur</p>
-                <p class="item product__price">2500 kr</p>
-                <div class="break"></div>
-                <p class="item product__desc">Armbandsur Tissot mekaniskt</p>
-            </div>
-            
-        </div>
-        <div class="product">
-            <img class="item-full product__img" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_large.png?format=webp&v=1530129458" alt="" srcset="">
-            <div class="product__info">
-                <p class="item product__title">Ring</p>
-                <p class="item product__price">2500 kr</p>
-                <div class="break"></div>
-                <p class="item product__desc">Armbandsur Tissot mekaniskt</p>
-            </div>
-            
-        </div>
-        <div class="product">
-            <img class="item-full product__img" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_large.png?format=webp&v=1530129458" alt="" srcset="">
-            <div class="product__info">
-                <p class="item product__title">Armbandsur</p>
-                <p class="item product__price">2500 kr</p>
-                <div class="break"></div>
-                <p class="item product__desc">Armbandsur Tissot mekaniskt</p>
-            </div>
-            
-        </div>
-        <div class="product">
-            <img class="item-full product__img" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_large.png?format=webp&v=1530129458" alt="" srcset="">
-            <div class="product__info">
-                <p class="item product__title">Armbandsur</p>
-                <p class="item product__price">2500 kr</p>
-                <div class="break"></div>
-                <p class="item product__desc">Armbandsur Tissot mekaniskt</p>
-            </div>
-            
-        </div>
+        <?php 
+        Render::renderProduct($klocka); 
+        
+        
+        ?>
     </div>
 
 </div>
